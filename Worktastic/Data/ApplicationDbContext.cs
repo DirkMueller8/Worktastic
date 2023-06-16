88d22fc4dc77e4f5d4f29fc52cfc0d5cbc350dcb
Dirk Mueller <dirkmuller8@outlook.de>
2023-06-16 13:40:56 +0200

    JobPosting Tabelle in Datenbank eingerichtet

--- Worktastic/Data/ApplicationDbContext.cs
+++ Worktastic/Data/ApplicationDbContext.cs
@@ -8,6 +8,7 @@ namespace Worktastic.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Models.JobPosting> JobPostings { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
